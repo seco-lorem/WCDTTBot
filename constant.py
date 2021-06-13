7fdb97b52aa0fb492f9b66b85b6c656ffb7e7a1c
seco-lorem <62249520+seco-lorem@users.noreply.github.com>
2021-06-13 20:06:35 +0300

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,11 +1,11 @@
 
-TARGET_SUBREDDIT = "MyTestSubSryToBother"
+TARGET_SUBREDDIT = ""
 
-CLIENT_ID       = "oMgEErAMaBw4GA"
-CLIENT_SECRET   = "39CMwX9JLoAFkV8rCy8dA3hLZco"
-USER_AGENT      = "I don't know what this is"
-USERNAME        = "ClubPast"
-PASSWORD        = "B))CHQc2Ba2fx=."
+CLIENT_ID       = ""
+CLIENT_SECRET   = ""
+USER_AGENT      = ""
+USERNAME        = ""
+PASSWORD        = ""
 ADD_PERR_RECAP  = 0
 INTOLERANCY_RATE= 4                         # How many weeks earn you a weak of inactivity pass.
 RANK_LIMITS     = [4, 20]                   # in weeks. 0-3 New, 4-19 Usual, 20+ Senior
