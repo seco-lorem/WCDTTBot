d66cf919e19a7fe9d5b91d21c0f31df05a9ba6ab
seco-lorem <62249520+seco-lorem@users.noreply.github.com>
2022-01-15 13:53:52 +0300

    seperated credentials from constant.py

--- constant.py
+++ constant.py
@@ -1,11 +1,13 @@
 
-TARGET_SUBREDDIT = ""                       # The Private subreddit
+import credentials
 
-CLIENT_ID       = ""
-CLIENT_SECRET   = ""
-USER_AGENT      = ""
-USERNAME        = ""
-PASSWORD        = ""
+TARGET_SUBREDDIT = ""                       # The Private Subreddit
+
+CLIENT_ID       = credentials.CLIENT_ID
+CLIENT_SECRET   = credentials.CLIENT_SECRET
+USER_AGENT      = credentials.USER_AGENT
+USERNAME        = credentials.USERNAME
+PASSWORD        = credentials.PASSWORD
 ADD_PERR_RECAP  = 100
 INTOLERANCY_RATE= 4                         # How many weeks earn you a weak of inactivity pass.
 RANK_LIMITS     = [4, 20]                   # in weeks. 0-3 New, 4-19 Usual, 20+ Senior
